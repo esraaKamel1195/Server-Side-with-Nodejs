2a77fba0e35938cbaf7793083b4a9b533b68649d
Esraa Kamel <esraa.kamel1811@gmail.com>
2021-05-01 23:40:24 +0200

    Express Generator REST API

--- confusionServer/app.js
+++ confusionServer/app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const dashRouter = require('./routes/dashRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 
 var app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use("/dashes", dashRouter);
+app.use("/promotions", promoRouter);
+app.use("/leaders", leaderRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

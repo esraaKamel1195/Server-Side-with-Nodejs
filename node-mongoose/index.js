190d5a88f21c9ccdeb19fc04f63860d474b68744
Esraa Kamel <esraa.kamel1811@gmail.com>
2021-05-10 13:51:48 +0200

    Mongoose Part 2

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -8,20 +8,22 @@ const connect = mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopolog
 connect.then((db) => {
     console.log('Connected correctly to server');
 
-    var newDash = Dashes({
+    Dashes.create({
         name: "Uthappizza",
         description: "test"
+    })
+    .then((dash) => {
+        console.log(dash);
+
+        return Dashes.find({}).exec();
+    }).then((dashes)=> {
+        console.log(dashes);
+
+        return Dashes.remove({});
+    }).then(() => {
+        
+        return mongoose.connection.close();
+    }).catch((err) => {
+        console.log(err);
     });
-    newDash.save()
-        .then((dash) => {
-            console.log(dash);
-            return Dashes.find({});
-        }).then((dashes)=> {
-            console.log(dashes);
-            return Dashes.remove({});
-        }).then(() => {
-            return mongoose.connection.close();
-        }).catch((err) => {
-            console.log(err);
-        });
 });

47348b1337aae69a71eeb1ed4a642c30d9a210a1
Esraa_Kamel <esraa.kamel1811@gmail.com>
2021-11-18 11:09:21 +0200

    Adding data to the server

--- Express-LL-Essential-course/app.js
+++ Express-LL-Essential-course/app.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import data from './data/data.json';
 const app = express();
 const Port = 3000;
 
-app.listen(Port, ()=>
-    console.log(`Your server is running on port ${Port}`)
-);
+app.listen(Port, ()=> {
+    console.log(`Your server is running on port ${Port}`);
+    console.log(data)
+});
